df4cca8cfdadca8d4d8a4cad4247d8ddd9144db4
Bayo Ade <bayo.300@gmail.com>
2020-04-22 17:01:34 +0100

    two different binary search algorithms

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,3 +1,6 @@
+console.clear()
+let log = console.log
+
 let data = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
 
 let binarySearch_iterative = function(data, target) {
@@ -18,9 +21,24 @@ let binarySearch_iterative = function(data, target) {
     return false
 }
 
+let binarySearch_recursive = function(data, target, low, high) {
+    if (low > high) {
+        return false
+    }
+
+    let mid = Math.floor((low + high / 2))
+
+    if (target == data[mid]) {
+        return true
+    } else if (target < data[mid]) {
+        return binarySearch_recursive(data, target, low, mid - 1)
+    } else {
+        return binarySearch_recursive(data, target, mid + 1, high)
+    }
+}
+
 let target = 25
 
-console.clear()
-let log = console.log
 
 log(binarySearch_iterative(data, target))
+log(binarySearch_iterative(data, target, 0, data.length - 1))
